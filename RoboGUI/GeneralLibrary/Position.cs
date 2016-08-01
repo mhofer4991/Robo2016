8b6ee1bb61c1a9ea0ecec3b91967967bf7f6a8c8
Michael Ploy <93216@fhwn.ac.at>
2016-08-01 14:53:42 +0200

    change @position

--- RoboGUI/GeneralLibrary/Position.cs
+++ RoboGUI/GeneralLibrary/Position.cs
@@ -9,12 +9,37 @@ namespace GeneralLibrary
     [Serializable()]
     public class Position
     {
+        private int x;
+        private int y;
+
         public Position(int x, int y)
         {
             this.X = x;
             this.Y = y;     
         }
-        public int X { get; set; }
-        public int Y { get; set; }
+
+        public int X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
     }
 }
